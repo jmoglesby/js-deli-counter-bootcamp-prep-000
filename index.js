a663e4c7f5148b8228a60abf1ed8ca6000f42b0b
Jeremy Oglesby <iaminaband@gmail.com>
2019-05-22 21:02:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ function takeANumber(line, newPerson) {
   line.push(newPerson);
   return `Welcome ${newPerson}. You are number ${line.length} in line.`;
 }
+
+function nowServing (line) {
+  var nextServed;
+  if (line.length > 0) {
+    nextServed = line.shift;
+  } else {
+    nextServed = "There is nobody waiting to be served.";
+  }
+  return nextServed;
+}
+
+function currentLine(line) {
+  var res = "The line is currently";
+  if (line.length > 0) {
+    var personList;
+    for (let i = 0; i < line.length; i++) {
+      var person = line[i]
+      personList = `${personList}, ${i + 1}. ${person}`;
+    }
+    res = `${res}: ${personList}`;
+  } else {
+    res = `${res} empty.`;
+  }
+  return res;
+}
