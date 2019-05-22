d7b4efbf41b4218eb2f4c4181aad80cff86a27e0
Jeremy Oglesby <iaminaband@gmail.com>
2019-05-22 21:04:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(line, newPerson) {
   line.push(newPerson);
-  return `Welcome ${newPerson}. You are number ${line.length} in line.`;
+  return `Welcome, ${newPerson}. You are number ${line.length} in line.`;
 }
 
 function nowServing (line) {
